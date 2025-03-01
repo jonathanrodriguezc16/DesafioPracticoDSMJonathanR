cbf804e7753e7980cfe0e5562184e4b5742a65ee
jonathanrodriguezc16 <jonathanrodriguezc16@gmail.com>
2025-03-01 11:13:50 -0600

    Creacion de pantalla de inicio para acceder a las diferentes activity

--- app/src/main/java/com/example/desafiopracticodsmjonathanr/MainActivity.kt
+++ app/src/main/java/com/example/desafiopracticodsmjonathanr/MainActivity.kt
@@ -1,20 +1,40 @@
 package com.example.desafiopracticodsmjonathanr
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var btnPromedio: Button
+    lateinit var btnSalario: Button
+    lateinit var btnCalculadora: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        btnPromedio=findViewById(R.id.btnPromedio)
+        btnSalario = findViewById(R.id.btnSalario)
+        btnCalculadora=findViewById(R.id.btnCalculadora)
+
+        btnPromedio.setOnClickListener {
+            val intent =Intent(this, PromedioActivity::class.java)
+            startActivity(intent)
         }
+        btnSalario.setOnClickListener {
+            val intent =Intent(this, SalarioActivity::class.java)
+            startActivity(intent)
+        }
+        btnCalculadora.setOnClickListener {
+            val intent =Intent(this, CalculadoraActivity::class.java)
+            startActivity(intent)
+        }
+
     }
 }
